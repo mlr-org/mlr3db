dff4ca2c6be6b0d4ffa64a2c400b9bc5fd1b1f40
Michel Lang <michellang@gmail.com>
2019-07-01 16:57:30 +0200

    new api

--- tests/testthat/test_experiment.R
+++ tests/testthat/test_experiment.R
@@ -1,21 +1,18 @@
-context("experiment")
-
 data = as_sqlite_tbl(iris)
 b = as_data_backend(data, "row_id")
 task = mlr3::TaskClassif$new("iris_sqlite", b, "Species")
 learner = mlr3::mlr_learners$get("classif.featureless")
 
-test_that("simple experiments work", {
-  e = mlr3::Experiment$new(task, learner)
-  expect_experiment(e)
-  e$train()$predict()$score()
-  expect_experiment(e)
-  expect_false(any(e$has_errors))
+test_that("single step train + predict", {
+  expect_learner(learner$train(task, 1:120))
+  p = learner$predict(task, 121:150)
+  expect_prediction(p)
+  expect_data_table(data.table::as.data.table(p), nrow = 30)
+  expect_character(learner$errors, len = 0L)
 })
 
 test_that("resample work", {
-  resampling = mlr3::mlr_resamplings$get("cv")
-  rr = mlr3::resample(task, learner, resampling)
+  rr = mlr3::resample(task, learner, "cv3")
   expect_resample_result(rr)
 })
 
