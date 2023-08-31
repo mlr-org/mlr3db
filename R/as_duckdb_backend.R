259db6aed24776bdb2c73a9e8c8d5faa895cbed4
Sebastian Fischer <sebf.fischer@gmail.com>
2023-08-31 14:09:06 +0200

    ...

--- R/as_duckdb_backend.R
+++ R/as_duckdb_backend.R
@@ -41,7 +41,7 @@ as_duckdb_backend.character = function(data, path = getOption("mlr3db.duckdb_dir
   query = "CREATE OR REPLACE VIEW 'mlr3db_view' AS SELECT *"
   if (is.null(primary_key)) {
     primary_key = "mlr3_row_id"
-    query = paste0(query, ", row_number() OVER () AS 'mlr3_row_id'")
+    query = paste0(query, ", row_number() OVER () AS mlr3_row_id")
   } else {
     assert_string(primary_key)
   }
